891a8022409ed2c36a772dd9246b92cd78ef64e3
ajay bilgaiyan <abilgaiyan@gmail.com>
2020-03-02 13:02:01 +0530

    form validation using finalform

--- client-mobx/src/features/home/HomePage.tsx
+++ client-mobx/src/features/home/HomePage.tsx
@@ -4,20 +4,20 @@ import { Link } from 'react-router-dom';
 
 const HomePage = () => {
   return (
-    <Segment inverted textAlign="center" vertical className="masthead">
+    <Segment inverted textAlign='center' vertical className='masthead'>
       <Container text>
-        <Header as="h1" inverted>
+        <Header as='h1' inverted>
           <Image
-            size="massive"
-            src="/assets/logo.png"
-            alt="logo"
+            size='massive'
+            src='/assets/logo.png'
+            alt='logo'
             style={{ marginBottom: 12 }}
           />
           Reactivities
         </Header>
-        <Header as="h2" inverted content="Welcome to My App" />
-        <Button as={Link} to="/activities" size="huge" inverted>
-          Take me to the App!
+        <Header as='h2' inverted content='Welcome to Reactivities' />
+        <Button as={Link} to='/activities' size='huge' inverted>
+          Take me to the activities!
         </Button>
       </Container>
     </Segment>
--- client-mobx/src/features/nav/NavBar.tsx
+++ client-mobx/src/features/nav/NavBar.tsx
@@ -5,19 +5,18 @@ import { NavLink } from 'react-router-dom';
 
 const NavBar: React.FC = () => {
   return (
-    <Menu fixed="top" inverted>
+    <Menu fixed='top' inverted>
       <Container>
-        <Menu.Item header as={NavLink} exact to="/">
-          <img src="/assets/logo.png" alt="logo" style={{ marginRight: 10 }} />
-          My React App
+        <Menu.Item header as={NavLink} exact to='/'>
+          <img src='/assets/logo.png' alt='logo' style={{ marginRight: 10 }} />
+          Reactivities
         </Menu.Item>
-        <Menu.Item name="Activities" as={NavLink} to="/activities" />
+        <Menu.Item name='Activities' as={NavLink} to='/activities' />
         <Menu.Item>
           <Button
-            as={NavLink}
-            to="/createActivity"
+            as={NavLink} to='/createActivity'
             positive
-            content="Create Activity"
+            content='Create Activity'
           />
         </Menu.Item>
       </Container>
